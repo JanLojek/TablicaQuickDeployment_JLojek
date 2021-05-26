e70a2e5af2370e8b31cc5453083c57f330b0d815
JanLojek <jan.lojek@loskarga.edu.pl>
2021-05-26 09:28:45 +0200

    Dodaj pliki projektów.

---
+++ TablicaQuickDeployment_JLojek/tablica.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<windows.h>
+
+using namespace std;
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(0);
+	int a = 0;
+	cout << "Ile razy? ";
+	cin >> a;
+	for(int i=0; i<a;++i)
+	system("start https://jamboard.google.com/d/1-iQS9RBNiodLyYRlgtGYA4STkbm58k7r9-zNWKzsQV0/viewer?f=8");
+
+
+	return 0;
+}
